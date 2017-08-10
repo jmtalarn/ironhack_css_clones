2aefdbd1612cf763ed57ec016ae2978c722df607
jmtalarn <joanmaria.talarn@idiada.com>
2017-08-10 12:13:48 +0200

    Lecture 2.7 Adding tabs with a bit of Javascript. Using vanulla Javascript instead of jQuery

---
+++ netflix/js/app.js
@@ -0,0 +1,18 @@
+(function() {
+  var tabs = Array.prototype.slice.call(document.querySelectorAll('#features > nav a'));
+  var tabsContent = Array.prototype.slice.call(document.querySelectorAll('#features > article > section'));
+  tabs.forEach(x=>{
+    x.addEventListener('click', function(e) {
+      e.preventDefault();
+      var that = this;
+      tabs.forEach(x=> x.className = x.className.replace('is-selected', ''));
+      that.className += ' is-selected';
+      tabsContent.forEach(x=>x.className = x.className.replace('is-selected', ''));
+      tabsContent
+        .filter(
+          (tab, i) => tab.dataset.id === that.dataset.id
+        )
+        .forEach(x=>x.className +='is-selected');
+    });
+  });
+})();
